d00b25a8a283aadbff6ac22090d597ce521ce080
Your Name <you@example.com>
2024-04-16 22:56:24 -0300

    atualizacao

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 const Home: React.FC = () => {
     return (
@@ -6,6 +6,6 @@ const Home: React.FC = () => {
             <h1>HomePage</h1>
         </div>
     );
-}
+};
 
 export default Home;
